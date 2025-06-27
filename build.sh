71ab1c87624ccaef8e4925c69edc832343727fa3
wbullaughey <wlb122@verizon.net>
2025-06-27 13:51:35 -0400

    temp branch changes 6-27-25

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-echo build.sh at `pwd`
 source ~/.zshrc
-#export GPR_PROJECT_PATH="ada_lib:aunit"
-alr -v build -- -j10 -s -k -gnatE
+export WHICH=$1
+
+# WHICH values
+#   all     - build everything (help_tests, driver unit tests, applications)
+#   execute    - build application or library for subdirectory level
+#   help_test  - builds help_test at level
+
+echo build WHICH $WHICH
+
+../global_build.sh $WHICH library
